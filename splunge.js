4f8b95264ddb9620d13e61f9feca62f452c817b8
Spencer Tipping <spencer@spencertipping.com>
2012-09-27 21:31:34 -0500

    More logic, and an almost-working demo

---
+++ splunge.js
@@ -0,0 +1,43 @@
+caterwaul.module( 'splunge' ,function($) { (function() {var tau=2*Math.PI,bbox=function(h) {;
+return(function() {var b=function(h,x0,y0,dy) {;
+return h.bbox?h.bbox
+:h.bbox= (function() {var ymax=h.xs? (function(xs) {var x,x0,xi,xl,xr;
+for(var x0= (0) ,xi=0,xl=xs.length;
+xi<xl;
+ ++xi)x=xs[xi] ,x0= (x0+x.value) ;
+return x0} ) .call(this,h.xs) 
+:0,y=0,new_dy=dy/ymax;
+return(h.xs&& (function(cs) {var c,c0,ci,cl,cr;
+for(var ci=0,cl=cs.length;
+ci<cl;
+ ++ci)c=cs[ci] , ( (b(c,x0+1,y,new_dy) ,y+=c.value*new_dy) ) ;
+return cs} ) .call(this,h.xs) , {x0:x0,dx:1+ (h.xs? (function(xs) {var x,x0,xi,xl,xr;
+for(var x0= (0) ,xi=0,xl=xs.length;
+xi<xl;
+ ++xi)x=xs[xi] ,x0= (Math.max(x0,x.bbox.dx) ) ;
+return x0} ) .call(this,h.xs) 
+:0) ,y0:y0,dy:dy*h.value} ) } ) .call(this) } ;
+return b(h,0,0,1) } ) .call(this) } ,base=function(b) {;
+return{x:b.x0,y:b.y0} } ,mix=function(a,b) {;
+return{x:a.x,y:b.y} } ,d=function(b) {;
+return{x:b.dx,y:b.dy} } ,project=function(b,x) {;
+return{x:b.x0+b.dx*x,y:b.y0+b.dy*x} } ,interpolate=function(t1,t2,x) {;
+return add(scale(t1,1.0-x) ,scale(t2,x) ) } ,add=function(t1,t2) {;
+return{x0:t1.x0+t2.x0,dx:t1.dx+t2.dx,y0:t1.y0+t2.y0,dy:t1.dy+t2.dy} } ,scale=function(t,x) {;
+return{x0:t.x0*x,dx:t.dx*x,y0:t.y0*x,dy:t.dy*x} } ,inverse=function(t,p) {;
+return{x:Math.tan( (Math.sqrt(p.x*p.x+p.y*p.y) -0.5) *tau*t.dx+t.x0) ,y:Math.atan2(p.x,p.y) *t.dy+t.y0} } ,transform=function(t,p) {;
+return(function() {var d=Math.atan( (p.x-t.x0) /t.dx) /tau+0.5,angle=Math.min(Math.max( (p.y-t.y0) /t.dy*tau,0) ,tau) ;
+return{x:d*Math.cos(angle) ,y:d*Math.sin(angle) ,d:d,angle:angle} } ) .call(this) } ,point_hits_desc=function(p,h) {;
+return(function(it) {return p.x>=it.x0&&p.y>=it.y0&&p.y<=it.y0+it.dy&&it} ) .call(this, (bbox(h) ) ) } ,point_hits_self=function(p,h) {;
+return(function(it) {return it&&p.x<=it.x0+it.dy} ) .call(this, (point_hits_desc(p,h) ) ) } ,arc_path=function(h,t) {;
+return function(c) {;
+return(function() {var p1=base(bbox(h) ) ,p2=project(bbox(h) ,1) ,c1=transform(t,p1) ,c3=transform(t,p2) ,c2=transform(t,mix(p2,p1) ) ,c4=transform(t,mix(p1,p2) ) ;
+return( ( ( (c.moveTo(c1.x,c1.y) ,c.lineTo(c2.x,c2.y) ) ,c.arc(0,0,c2.d,c2.angle,c3.angle) ) ,c.lineTo(c4.x,c4.y) ) ,c.arc(0,0,c4.d,c4.angle,c1.angle,true) ) } ) .call(this) } } ,path=function(h,t) {;
+return function(c,f) {;
+return( (arc_path(h,t) (c) ,f(c,h) ) ,h.xs&& (function(xs) {var x,x0,xi,xl,xr;
+for(var xi=0,xl=xs.length;
+xi<xl;
+ ++xi)x=xs[xi] , (path(x,t) (c,f) ) ;
+return xs} ) .call(this,h.xs) ) } } ;
+return $.splunge=function(data) {;
+return path} } ) .call(this) } ) ;
